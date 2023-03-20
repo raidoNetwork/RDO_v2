784c0829c57a34278343ffdc01457d5afd548de3
Simon Metelev <semen.metelev@gmail.com>
2023-03-20 13:01:39 +0100

    Fix up staking

--- generator/service.go
+++ generator/service.go
@@ -73,14 +73,7 @@ func (s *Service) GenerateUnsafeStakeTx(fee uint64, hexKey string, amount uint64
 		return transaction, err
 	}
 
-	// Check if the node is a validator
-	err = s.attestation.IsNodeValidator(address.Hex())
-	if err == nil {
-		// The address is a validator so the node can only be the BlackholeAddress
-		if node != common.BlackHoleAddress {
-			return nil, errors.New("Validators can only stake on the BlackHoleAddress")
-		}
-	} else {
+	if node != common.BlackHoleAddress {
 		// The address is not a validator so the node has to be a validator
 		err = s.attestation.IsNodeValidator(node)
 		if err != nil {
@@ -211,13 +204,7 @@ func (s *Service) GenerateStakeTx(fee uint64, addr string, amount uint64, node s
 		typedTx := types.NewTransaction(transaction)
 
 		// Check if the node is a validator
-		err = s.attestation.IsNodeValidator(addr)
-		if err == nil {
-			// The address is a validator so the node can only be the BlackholeAddress
-			if node != common.BlackHoleAddress {
-				return nil, errors.New("Validators can only stake on the BlackHoleAddress")
-			}
-		} else {
+		if node != common.BlackHoleAddress {
 			// The address is not a validator so the node has to be a validator
 			err = s.attestation.IsNodeValidator(node)
 			if err != nil {
